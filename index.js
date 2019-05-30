fa42856c14324838683f95989960d963193cda08
Karl Xu <karl.yogi.xu@gmail.com>
2019-05-30 22:42:56 +0800

    write log to loki

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 const port = 3001
+const loki_url = 'http://localhost:3100/api/prom/push'
+const host = 'somehost'
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/loki', (req, res) => {
+  headers = {
+    'Content-type': 'application/json'
+  }
+  payload = {
+    'streams': [
+      {
+        'labels': '{source=\"nodejs\"}',
+        'entries': [
+          {
+            'ts': new Date(),
+            'line': 'This is a warning.'
+          }
+        ]
+      }
+    ]
+  }
+  axios.post(loki_url, payload, {headers : headers}).then((res) => {
+    console.log(`statusCode: ${res.statusCode}`)
+    console.log(res)
+  })
+  .then((response) => {
+    console.log(response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
